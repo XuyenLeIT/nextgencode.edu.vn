0fe841fba04a89c82bef88a488510ad0517ecf77
XuyenLe <xuyenleit@gmail.com>
2024-09-07 11:37:15 +0700

    add icon

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Course management')</title>
+    <link rel="icon" href="logo/edu.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
@@ -50,7 +51,6 @@
                 </div>
             </div>
         </div>
-        {{-- @dd($isHomeUrl) --}}
         @if ($isHomeUrl)
             @if ($carausels->count() > 0)
                 <div class="carousel-home" data-flickity='{ "freeScroll": true, "wrapAround": true,"autoPlay": true}'>
